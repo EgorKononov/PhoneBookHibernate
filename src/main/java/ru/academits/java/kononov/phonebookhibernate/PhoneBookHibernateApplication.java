3612dc00cde97b80e208ec37dc13f098c8ebf346
EgorKononov <egor.academ@gmail.com>
2024-08-24 23:28:13 +0700

    Initial commit

---
+++ src/main/java/ru/academits/java/kononov/phonebookhibernate/PhoneBookHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.academits.java.kononov.phonebookhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookHibernateApplication.class, args);
+    }
+
+}
